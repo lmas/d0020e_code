365d88113dede42c3abf6206e73bc3cc2044ae74
Alex <git@larus.se>
2025-01-06 19:24:04 +0100

    Adds response body to the first tests

--- Comfortstat/api_fetch_test.go
+++ Comfortstat/api_fetch_test.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"net/http"
+	"strings"
 	"testing"
+	"time"
 )
 
 // mockTransport is used for replacing the default network Transport (used by
@@ -30,14 +34,27 @@ func (t mockTransport) domainHits(domain string) int {
 	return -1
 }
 
+// TODO: this might need to be expanded to a full JSON array?
+const priceExample string = `[{
+	"SEK_per_kWh": 0.26673,
+	"EUR_per_kWh": 0.02328,
+	"EXR": 11.457574,
+	"time_start": "2025-01-06T%02d:00:00+01:00",
+	"time_end": "2025-01-06T%02d:00:00+01:00"
+}]`
+
 // RoundTrip method is required to fulfil the RoundTripper interface (as required by the DefaultClient).
 // It prevents the request from being sent over the network and count how many times
 // a domain was requested.
 func (t mockTransport) RoundTrip(req *http.Request) (resp *http.Response, err error) {
+	hour := time.Now().Local().Hour()
+	fakeBody := fmt.Sprintf(priceExample, hour, hour+1)
+	// TODO: should be able to adjust these return values for the error cases
 	resp = &http.Response{
 		Status:     "200 OK",
 		StatusCode: 200,
 		Request:    req,
+		Body:       io.NopCloser(strings.NewReader(fakeBody)),
 	}
 	t.hits[req.URL.Hostname()] += 1
 	return
@@ -49,8 +66,8 @@ const apiDomain string = "www.elprisetjustnu.se"
 
 func TestAPIDataFetchPeriod(t *testing.T) {
 	want := 3600
-	if apiFetchPeriod != want {
-		t.Errorf("expected API fetch period = %d, got %d", want, apiFetchPeriod)
+	if apiFetchPeriod < want {
+		t.Errorf("expected API fetch period >= %d, got %d", want, apiFetchPeriod)
 	}
 }
 
@@ -59,10 +76,14 @@ func TestSingleUnitAssetOneAPICall(t *testing.T) {
 	// Creates a single UnitAsset and assert it only sends a single API request
 	ua := initTemplate().(*UnitAsset)
 	retrieveAPI_price(ua)
+
+	// TEST CASE: cause a single API request
 	hits := trans.domainHits(apiDomain)
 	if hits > 1 {
 		t.Errorf("expected number of api requests = 1, got %d requests", hits)
 	}
+
+	// TODO: try more test cases!
 }
 
 func TestMultipleUnitAssetOneAPICall(t *testing.T) {
@@ -73,8 +94,12 @@ func TestMultipleUnitAssetOneAPICall(t *testing.T) {
 		ua := initTemplate().(*UnitAsset)
 		retrieveAPI_price(ua)
 	}
+
+	// TEST CASE: causing only one API hit while using multiple UnitAssets
 	hits := trans.domainHits(apiDomain)
 	if hits > 1 {
 		t.Errorf("expected number of api requests = 1, got %d requests (from %d units)", hits, units)
 	}
+
+	// TODO: more test cases??
 }
