98498b96f82389593f99c3bd83cad5e30408a823
Simon Pergel <isoepe-1@student.ltu.se>
2025-01-22 11:22:55 +0100

    new test file added

---
+++ Comfortstat/Comfortstat_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func Testtemprature(t *testing.T) {
+
+}
+
+// #1 Test if struc can handle floats
+// #2 Test if we trys to update the struct, that infact the value is updated correctly
+// #3
