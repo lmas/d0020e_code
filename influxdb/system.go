ab51c5a2d2c355a6134da431804eb07d904dab70
Alex <git@larus.se>
2025-01-24 18:17:12 +0100

    Cleans up the example unitasset and system

---
+++ influxdb/system.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"sync"
+
+	"github.com/sdoque/mbaigo/components"
+	"github.com/sdoque/mbaigo/usecases"
+)
+
+func main() {
+	sys := newSystem()
+	sys.loadConfiguration()
+	sys.listenAndServe()
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+// There's no interface to use, so have to encapsulate the base struct instead
+type system struct {
+	components.System
+
+	cancel   func()
+	cleanups []func()
+}
+
+func newSystem() (sys *system) {
+	// Handle graceful shutdowns using this context. It should always be canceled,
+	// no matter the final execution path so all computer resources are freed up.
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// Create a new Eclipse Arrowhead application system and then wrap it with a
+	// "husk" (aka a wrapper or shell), which then sets up various properties and
+	// operations that's required of an Arrowhead system.
+	// var sys system
+	sys = &system{
+		System: components.NewSystem("influxdb", ctx),
+		cancel: cancel,
+	}
+	sys.Husk = &components.Husk{
+		Description: "collects data from other Arrorhead systems and sends it to a InfluxDB server.",
+		Details:     map[string][]string{"Developer": {"Alex"}},
+		ProtoPort:   map[string]int{"https": 8691, "http": 8690, "coap": 0},
+		InfoLink:    "https://github.com/lmas/d0020e_code/tree/master/influxdb",
+	}
+	return
+}
+
+func (sys *system) loadConfiguration() {
+	// Try loading the config file (in JSON format) for this deployment,
+	// by using a unit asset with default values.
+	uat := initTemplate()
+	sys.UAssets[uat.GetName()] = &uat
+	rawUAs, servsTemp, err := usecases.Configure(&sys.System)
+	// If the file is missing, a new config will be created and an error is returned here.
+	if err != nil {
+		log.Fatalf("Error while reading configuration: %v\n", err)
+	}
+
+	// Load the proper unit asset(s) using the user-defined settings from the config file.
+	clear(sys.UAssets)
+	for _, raw := range rawUAs {
+		var uac unitAsset
+		if err := json.Unmarshal(raw, &uac); err != nil {
+			log.Fatalf("Error while unmarshalling configuration: %+v\n", err)
+		}
+		ua, cleanup := newUnitAsset(uac, &sys.System, servsTemp)
+		sys.UAssets[ua.GetName()] = &ua
+		sys.cleanups = append(sys.cleanups, cleanup)
+	}
+}
+
+func (sys *system) listenAndServe() {
+	var wg sync.WaitGroup // Used for counting all started goroutines
+	// Generate PKI keys and CSR to obtain a authentication certificate from the CA
+	usecases.RequestCertificate(&sys.System)
+	// Register the system and its services
+	// WARN: this func runs a goroutine of it's own, which makes it hard to count using the waitgroup (and I can't be arsed to do it properly...)
+	usecases.RegisterServices(&sys.System)
+	go func() {
+		wg.Add(1)
+		// start a web server and serve the request handlers in the unit assets
+		err := usecases.SetoutServers(&sys.System)
+		if err != nil {
+			log.Println("Error while running web server:", err)
+			sys.cancel()
+		}
+		wg.Done()
+	}()
+
+	// Run any other goroutines here!
+
+	<-sys.Sigs // Block and wait for the shutdown signal (ctrl+c)
+	log.Println("Shutting down system and waiting for the goroutines to terminate")
+	// Gracefully terminate any goroutines and wait for them to shutdown properly, before doing any cleanups
+	sys.cancel()
+	wg.Wait()
+	for _, f := range sys.cleanups {
+		f()
+	}
+}
--- influxdb/unitasset.go
+++ influxdb/unitasset.go
@@ -5,13 +5,14 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/sdoque/mbaigo/components"
 )
 
 // A UnitAsset models an interface or API for a smaller part of a whole system, for example a single temperature sensor.
 // This type must implement the go interface of "components.UnitAsset"
-type UnitAsset struct {
+type unitAsset struct {
 	// Public fields
 	// TODO: Why have these public and then provide getter methods? Might need refactor..
 	Name        string              `json:"name"`    // Must be a unique name, ie. a sensor ID
@@ -20,85 +21,64 @@ type UnitAsset struct {
 	ServicesMap components.Services `json:"-"`
 	CervicesMap components.Cervices `json:"-"`
 
-	// Internal fields this UA might need to perform it's function, example:
-	temperature float64
+	// Internal fields this UA might need to perform it's function
 }
 
-func (ua *UnitAsset) GetName() string {
+func (ua *unitAsset) GetName() string {
 	return ua.Name
 }
 
-func (ua *UnitAsset) GetDetails() map[string][]string {
+func (ua *unitAsset) GetDetails() map[string][]string {
 	return ua.Details
 }
 
 // GetServices returns all services and capabilities this UnitAsset is providing to consumers.
-func (ua *UnitAsset) GetServices() components.Services {
+func (ua *unitAsset) GetServices() components.Services {
 	return ua.ServicesMap
 }
 
 // GetCervices returns the list of services that is being consumed by this UnitAsset.
-func (ua *UnitAsset) GetCervices() components.Cervices {
+func (ua *unitAsset) GetCervices() components.Cervices {
 	return ua.CervicesMap
 }
 
 // ensure UnitAsset implements the components.UnitAsset interface (this check is done at compile time)
-var _ components.UnitAsset = (*UnitAsset)(nil)
+var _ components.UnitAsset = (*unitAsset)(nil)
 
 ////////////////////////////////////////////////////////////////////////////////
 
 // initTemplate initializes a new UA and prefils it with some default values.
 // The returned instance is used for generating the configuration file, whenever it's missing.
 func initTemplate() components.UnitAsset {
-	// First predefine any exposed services
-	// (see https://github.com/sdoque/mbaigo/blob/main/components/service.go for documentation)
-	temperature := components.Service{
-		Definition:  "temperature-def",                             // TODO: this get's incorrectly linked to the below subpath
-		SubPath:     "temperature-sub",                             // TODO: this path needs to be setup in Serving() too
-		Details:     map[string][]string{"Forms": {"SignalA_v1a"}}, // TODO: why this form here??
-		RegPeriod:   30,
-		Description: "provides the current temperature of this sensor (using a GET request)",
-	}
-
-	return &UnitAsset{
-		// TODO: These fields should reflect a unique asset (ie, a single sensor with unique ID and location)
-		Name: "temperature-UA",
-		Details: map[string][]string{
-			"Unit":     {"Celsius"},
-			"Location": {"Kitchen"},
-		},
-		// Don't forget to map the provided services from above!
-		ServicesMap: components.Services{
-			temperature.SubPath: &temperature,
-		},
+	return &unitAsset{
+		Name: "InfluxDB collector",
 	}
 }
-
-////////////////////////////////////////////////////////////////////////////////
 
 // newUnitAsset creates a new and proper instance of UnitAsset, using settings and
 // values loaded from an existing configuration file.
 // This function returns an UA instance that is ready to be published and used,
 // aswell as a function that can perform any cleanup when the system is shutting down.
-func newUnitAsset(uac UnitAsset, sys *components.System, servs []components.Service) (components.UnitAsset, func()) {
-	ua := &UnitAsset{
-		// Filling in public fields using the given data
+func newUnitAsset(uac unitAsset, sys *components.System, servs []components.Service) (components.UnitAsset, func()) {
+	ua := &unitAsset{
 		Name:        uac.Name,
 		Owner:       sys,
 		Details:     uac.Details,
 		ServicesMap: components.CloneServices(servs),
-
-		// Setting the example variable
-		temperature: 3.14,
 	}
-
-	// Optionally start background tasks here! Example:
-	go func() {
-		log.Println("Starting up " + ua.Name)
-	}()
-
 	// Returns the loaded unit asset and an function to handle optional cleanup at shutdown
 	return ua, func() {
 		log.Println("Cleaning up " + ua.Name)
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+// Serving maps the requested service paths with any request handlers.
+func (ua *unitAsset) Serving(w http.ResponseWriter, r *http.Request, servicePath string) {
+	switch servicePath {
+	default:
+		// TODO: should instead tell the visitor that there's no services published?
+		http.Error(w, "Invalid service request", http.StatusBadRequest)
 	}
 }
