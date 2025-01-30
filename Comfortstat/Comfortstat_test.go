b2dbfe2d360c6609feaba6f6b7529be73d6a6312
Simon Pergel <isoepe-1@student.ltu.se>
2025-01-30 18:38:01 +0100

    test for things.go is completed

--- Comfortstat/Comfortstat_test.go
+++ Comfortstat/Comfortstat_test.go
@@ -1,24 +1,54 @@
 package main
 
-/*
-func Test_structupdate(t *testing.T) {
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
 
-	asset := UnitAsset{
-		Min_temp: 20.0,
-	}
-	// Simulate the input signal
-	inputSignal := forms.SignalA_v1a{
-		Value: 17.0,
-	}
-	// Call the setMin_temp function
-	asset.setMin_temp(inputSignal)
+func Test_set_SEKprice(t *testing.T) {
+	ua := initTemplate().(*UnitAsset)
 
-	// Check if Min_temp is updated correctly
-	if asset.Min_temp != 17.0 {
-		t.Errorf("expected Min_temp to be 17.0, got %f", asset.Min_temp)
+	//Good case test: GET
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "http://localhost:8670/Comfortstat/Set%20Values/SEK_price", nil)
+	good_code := 200
+
+	ua.set_SEKprice(w, r)
+
+	resp := w.Result()
+	body, _ := io.ReadAll(resp.Body)
+
+	value := strings.Contains(string(body), `"value": 1.5`)
+	unit := strings.Contains(string(body), `"unit": "SEK"`)
+	version := strings.Contains(string(body), `"version": "SignalA_v1.0"`)
+
+	if resp.StatusCode != good_code {
+		t.Errorf("expected good status code: %v, got %v", good_code, resp.StatusCode)
 	}
+
+	if value != true {
+		t.Errorf("expected the statment to be true!")
+
+	}
+	if unit != true {
+		t.Errorf("expected the unit statement to be true!")
+	}
+	if version != true {
+		t.Errorf("expected the version statment to be true!")
+	}
+	// Bad test case: default part of code
+	w = httptest.NewRecorder()
+	r = httptest.NewRequest("123", "http://localhost:8670/Comfortstat/Set%20Values/SEK_price", nil)
+
+	ua.set_SEKprice(w, r)
+
+	resp = w.Result()
+
+	if resp.StatusCode != http.StatusNotFound {
+		t.Errorf("Expected the status to be bad but got: %v", resp.StatusCode)
+	}
+
 }
-*/
-// #1 Test if struc can handle floats
-// #2 Test if we trys to update the struct, that infact the value is updated correctly
-// #3
