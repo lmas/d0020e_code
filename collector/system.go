bb11160cc7ab577d4b29b3f6b1e69619649fda5f
Alex <git@larus.se>
2025-02-03 20:42:49 +0100

    issue #60: finalises the system setup

--- collector/system.go
+++ collector/system.go
@@ -13,6 +13,16 @@ import (
 func main() {
 	sys := newSystem()
 	sys.loadConfiguration()
+
+	// Generate PKI keys and CSR to obtain a authentication certificate from the CA
+	usecases.RequestCertificate(&sys.System)
+
+	// Register the system and its services
+	// WARN: this func runs a goroutine of it's own, which makes it hard to count
+	// using the waitgroup (and I can't be arsed to do it properly...)
+	usecases.RegisterServices(&sys.System)
+
+	// Run forever
 	sys.listenAndServe()
 }
 
@@ -23,7 +33,7 @@ type system struct {
 	components.System
 
 	cancel   func()
-	cleanups []func()
+	startups []func() error
 }
 
 func newSystem() (sys *system) {
@@ -56,6 +66,8 @@ func (sys *system) loadConfiguration() {
 	rawUAs, servsTemp, err := usecases.Configure(&sys.System)
 	// If the file is missing, a new config will be created and an error is returned here.
 	if err != nil {
+		// TODO: it would had been nice to catch the exact error for "created config.."
+		// and not display it as an actuall error, per se.
 		log.Fatalf("Error while reading configuration: %v\n", err)
 	}
 
@@ -66,38 +78,45 @@ func (sys *system) loadConfiguration() {
 		if err := json.Unmarshal(raw, &uac); err != nil {
 			log.Fatalf("Error while unmarshalling configuration: %+v\n", err)
 		}
-		ua, cleanup := newUnitAsset(uac, &sys.System, servsTemp)
+		ua, startup := newUnitAsset(uac, &sys.System, servsTemp)
 		sys.UAssets[ua.GetName()] = &ua
-		sys.cleanups = append(sys.cleanups, cleanup)
+		sys.startups = append(sys.startups, startup)
 	}
 }
 
 func (sys *system) listenAndServe() {
 	var wg sync.WaitGroup // Used for counting all started goroutines
-	// Generate PKI keys and CSR to obtain a authentication certificate from the CA
-	usecases.RequestCertificate(&sys.System)
-	// Register the system and its services
-	// WARN: this func runs a goroutine of it's own, which makes it hard to count using the waitgroup (and I can't be arsed to do it properly...)
-	usecases.RegisterServices(&sys.System)
+
+	// start a web server that serves basic documentation of the system
+	wg.Add(1)
 	go func() {
-		wg.Add(1)
-		// start a web server and serve the request handlers in the unit assets
-		err := usecases.SetoutServers(&sys.System)
-		if err != nil {
+		if err := usecases.SetoutServers(&sys.System); err != nil {
 			log.Println("Error while running web server:", err)
 			sys.cancel()
 		}
 		wg.Done()
 	}()
 
-	// Run any other goroutines here!
+	// Run all the startups in separate goroutines and keep track of them
+	for _, f := range sys.startups {
+		wg.Add(1)
+		go func(start func() error) {
+			if err := start(); err != nil {
+				log.Printf("Error while running collector: %s\n", err)
+				sys.cancel()
+			}
+			wg.Done()
+		}(f)
+	}
 
-	<-sys.Sigs // Block and wait for the shutdown signal (ctrl+c)
-	log.Println("Shutting down system and waiting for the goroutines to terminate")
-	// Gracefully terminate any goroutines and wait for them to shutdown properly, before doing any cleanups
+	// Block and wait for either a...
+	select {
+	case <-sys.Sigs: // user initiated shutdown signal (ctrl+c) or a...
+	case <-sys.Ctx.Done(): // shutdown request from a worker
+	}
+
+	// Gracefully terminate any leftover goroutines and wait for them to shutdown properly
+	log.Println("Initiated shutdown, waiting for workers to terminate")
 	sys.cancel()
 	wg.Wait()
-	for _, f := range sys.cleanups {
-		f()
-	}
 }
--- collector/unitasset.go
+++ collector/unitasset.go
@@ -4,8 +4,10 @@ package main
 // https://github.com/sdoque/systems/blob/main/ds18b20/thing.go
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/sdoque/mbaigo/components"
 )
@@ -14,7 +16,7 @@ import (
 // for example a single temperature sensor.
 // This type must implement the go interface of "components.UnitAsset"
 //
-// This unit asset represents this system's local cache, where all data from the
+// This unit asset represents a room's statistics cache, where all data from the
 // other system are gathered, before being sent off to the InfluxDB service.
 type unitAsset struct {
 	// Public fields
@@ -22,11 +24,18 @@ type unitAsset struct {
 	Name        string              `json:"name"`    // Must be a unique name, ie. a sensor ID
 	Owner       *components.System  `json:"-"`       // The parent system this UA is part of
 	Details     map[string][]string `json:"details"` // Metadata or details about this UA
-	ServicesMap components.Services `json:"-"`
-	CervicesMap components.Cervices `json:"-"`
+	ServicesMap components.Services `json:"-"`       // Services provided to consumers
+	CervicesMap components.Cervices `json:"-"`       // Services being consumed
 
 	// Internal fields this UA might need to perform it's function
+	InfluxDBHost     string `json:"influxdb_host"`
+	InfluxDBToken    string `json:"influxdb_token"`
+	CollectionPeriod int    `json:"collection_period"`
 }
+
+// Following methods are required by the interface components.UnitAsset.
+// Enforce a compile-time check that the interface is implemented correctly.
+var _ components.UnitAsset = (*unitAsset)(nil)
 
 func (ua *unitAsset) GetName() string {
 	return ua.Name
@@ -36,53 +45,124 @@ func (ua *unitAsset) GetDetails() map[string][]string {
 	return ua.Details
 }
 
-// GetServices returns all services and capabilities this UnitAsset is providing to consumers.
 func (ua *unitAsset) GetServices() components.Services {
 	return ua.ServicesMap
 }
 
-// GetCervices returns the list of services that is being consumed by this UnitAsset.
 func (ua *unitAsset) GetCervices() components.Cervices {
 	return ua.CervicesMap
 }
 
-// ensure UnitAsset implements the components.UnitAsset interface (this check is done at compile time)
-var _ components.UnitAsset = (*unitAsset)(nil)
+func (ua *unitAsset) Serving(w http.ResponseWriter, r *http.Request, servicePath string) {
+	// We don't provide any services!
+	http.Error(w, "No services available", http.StatusNotImplemented)
+}
 
 ////////////////////////////////////////////////////////////////////////////////
+
+const uaName string = "Cache"
 
 // initTemplate initializes a new UA and prefils it with some default values.
 // The returned instance is used for generating the configuration file, whenever it's missing.
 func initTemplate() components.UnitAsset {
 	return &unitAsset{
-		Name: "Cache",
+		Name:    uaName,
+		Details: map[string][]string{"Location": {"Kitchen"}},
+
+		InfluxDBHost:     "localhost:", // TODO: add port
+		InfluxDBToken:    "insert secret token here",
+		CollectionPeriod: 30,
 	}
 }
 
 // newUnitAsset creates a new and proper instance of UnitAsset, using settings and
 // values loaded from an existing configuration file.
 // This function returns an UA instance that is ready to be published and used,
-// aswell as a function that can perform any cleanup when the system is shutting down.
-func newUnitAsset(uac unitAsset, sys *components.System, servs []components.Service) (components.UnitAsset, func()) {
+// aswell as a function that can ...
+// TODO: complete doc and remove servs here and in the system file
+func newUnitAsset(uac unitAsset, sys *components.System, servs []components.Service) (components.UnitAsset, func() error) {
+	// Lost of consumed services
+	sProtocols := components.SProtocols(sys.Husk.ProtoPort)
+	temp := &components.Cervice{
+		Name:   "temperature",
+		Protos: sProtocols,
+		Url:    make([]string, 0),
+	}
+	price := &components.Cervice{
+		Name:   "SEK_price",
+		Protos: sProtocols,
+		Url:    make([]string, 0),
+	}
+	desired := &components.Cervice{
+		Name:   "desired_temp",
+		Protos: sProtocols,
+		Url:    make([]string, 0),
+	}
+	setpoint := &components.Cervice{
+		Name:   "setpoint",
+		Protos: sProtocols,
+		Url:    make([]string, 0),
+	}
+
 	ua := &unitAsset{
-		Name:        uac.Name,
-		Owner:       sys,
-		Details:     uac.Details,
-		ServicesMap: components.CloneServices(servs),
+		Name:    uac.Name,
+		Owner:   sys,
+		Details: uac.Details,
+		// ServicesMap: components.CloneServices(servs), // TODO: not required?
+		CervicesMap: components.Cervices{
+			temp.Name:     temp,
+			price.Name:    price,
+			desired.Name:  desired,
+			setpoint.Name: setpoint,
+		},
+
+		InfluxDBHost:     uac.InfluxDBHost,
+		InfluxDBToken:    uac.InfluxDBToken,
+		CollectionPeriod: uac.CollectionPeriod,
+		// TODO: other internal fields
 	}
+
+	// TODO: required for matching values with locations?
+	// ua.CervicesMap["temperature"].Details = components.MergeDetails(ua.Details, ref.Details)
+	// for _, cs := range ua.CervicesMap {
+	// TODO: or merge it with an empty map if this doesn't work...
+	// cs.Details = ua.Details
+	// }
+
 	// Returns the loaded unit asset and an function to handle optional cleanup at shutdown
-	return ua, func() {
-		log.Println("Cleaning up " + ua.Name)
-	}
+	return ua, ua.startup
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-// Serving maps the requested service paths with any request handlers.
-func (ua *unitAsset) Serving(w http.ResponseWriter, r *http.Request, servicePath string) {
-	switch servicePath {
-	default:
-		// TODO: should instead tell the visitor that there's no services published?
-		http.Error(w, "Invalid service request", http.StatusBadRequest)
+var errTooShortPeriod error = fmt.Errorf("collection period less than 1 second")
+
+func (ua *unitAsset) startup() (err error) {
+	if ua.CollectionPeriod < 1 {
+		return errTooShortPeriod
 	}
+
+	for {
+		select {
+		// Wait for a shutdown signal
+		case <-ua.Owner.Ctx.Done():
+			ua.cleanup()
+			return
+
+			// Wait until it's time to collect new data
+		case <-time.Tick(time.Duration(ua.CollectionPeriod) * time.Second):
+			if err = ua.collect(); err != nil {
+				return
+			}
+		}
+	}
+}
+
+func (ua *unitAsset) cleanup() {
+	// TODO: remove this func altogether, if it's not required later on
+}
+
+func (ua *unitAsset) collect() (err error) {
+	log.Println("tick")
+	return nil
 }
