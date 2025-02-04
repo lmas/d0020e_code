960277088c1317e75a1ea408bb95700e66455283
Simon Pergel <isoepe-1@student.ltu.se>
2025-02-04 11:31:34 +0100

    changed the name to a more suitable name

---
+++ Comfortstat/things_test.go
@@ -0,0 +1,478 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/sdoque/mbaigo/components"
+	"github.com/sdoque/mbaigo/forms"
+)
+
+// mockTransport is used for replacing the default network Transport (used by
+// http.DefaultClient) and it will intercept network requests.
+
+type mockTransport struct {
+	resp *http.Response
+	hits map[string]int
+}
+
+func newMockTransport(resp *http.Response) mockTransport {
+	t := mockTransport{
+		resp: resp,
+		hits: make(map[string]int),
+	}
+	// Highjack the default http client so no actuall http requests are sent over the network
+	http.DefaultClient.Transport = t
+	return t
+}
+
+// domainHits returns the number of requests to a domain (or -1 if domain wasn't found).
+
+func (t mockTransport) domainHits(domain string) int {
+	for u, hits := range t.hits {
+		if u == domain {
+			return hits
+		}
+	}
+	return -1
+}
+
+// price example string in a JSON-like format
+var priceExample string = fmt.Sprintf(`[{
+		"SEK_per_kWh": 0.26673,
+		"EUR_per_kWh": 0.02328,
+		"EXR": 11.457574,
+		"time_start": "%d-%02d-%02dT%02d:00:00+01:00",
+		"time_end": "2025-01-06T04:00:00+01:00"
+		}]`, time.Now().Local().Year(), int(time.Now().Local().Month()), time.Now().Local().Day(), time.Now().Local().Hour(),
+)
+
+// RoundTrip method is required to fulfil the RoundTripper interface (as required by the DefaultClient).
+// It prevents the request from being sent over the network and count how many times
+// a domain was requested.
+
+func (t mockTransport) RoundTrip(req *http.Request) (resp *http.Response, err error) {
+	t.hits[req.URL.Hostname()] += 1
+	t.resp.Request = req
+	return t.resp, nil
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+const apiDomain string = "www.elprisetjustnu.se"
+
+func TestAPIDataFetchPeriod(t *testing.T) {
+	want := 3600
+	if apiFetchPeriod < want {
+		t.Errorf("expected API fetch period >= %d, got %d", want, apiFetchPeriod)
+	}
+}
+
+func TestSingleUnitAssetOneAPICall(t *testing.T) {
+	resp := &http.Response{
+		Status:     "200 OK",
+		StatusCode: 200,
+		//Body:       io.NopCloser(strings.NewReader(fakeBody)),
+	}
+	trans := newMockTransport(resp)
+	// Creates a single UnitAsset and assert it only sends a single API request
+	ua := initTemplate().(*UnitAsset)
+	retrieveAPI_price(ua)
+
+	// TEST CASE: cause a single API request
+	hits := trans.domainHits(apiDomain)
+	if hits > 1 {
+		t.Errorf("expected number of api requests = 1, got %d requests", hits)
+	}
+}
+
+func TestMultipleUnitAssetOneAPICall(t *testing.T) {
+	resp := &http.Response{
+		Status:     "200 OK",
+		StatusCode: 200,
+	}
+	trans := newMockTransport(resp)
+	// Creates multiple UnitAssets and monitor their API requests
+	units := 10
+	for i := 0; i < units; i++ {
+		ua := initTemplate().(*UnitAsset)
+		retrieveAPI_price(ua)
+	}
+
+	// TEST CASE: causing only one API hit while using multiple UnitAssets
+	hits := trans.domainHits(apiDomain)
+	if hits > 1 {
+		t.Errorf("expected number of api requests = 1, got %d requests (from %d units)", hits, units)
+	}
+}
+
+func TestSetmethods(t *testing.T) {
+
+	asset := initTemplate().(*UnitAsset)
+
+	// Simulate the input signals
+	MinTemp_inputSignal := forms.SignalA_v1a{
+		Value: 1.0,
+	}
+	MaxTemp_inputSignal := forms.SignalA_v1a{
+		Value: 29.0,
+	}
+	MinPrice_inputSignal := forms.SignalA_v1a{
+		Value: 2.0,
+	}
+	MaxPrice_inputSignal := forms.SignalA_v1a{
+		Value: 12.0,
+	}
+	DesTemp_inputSignal := forms.SignalA_v1a{
+		Value: 23.7,
+	}
+
+	//call and test min_temp
+	asset.setMin_temp(MinTemp_inputSignal)
+	if asset.Min_temp != 1.0 {
+		t.Errorf("expected Min_temp to be 1.0, got %f", asset.Min_temp)
+	}
+	// call and test max_temp
+	asset.setMax_temp(MaxTemp_inputSignal)
+	if asset.Max_temp != 29.0 {
+		t.Errorf("expected Max_temp to be 25.0, got %f", asset.Max_temp)
+	}
+	//call and test Min_price
+	asset.setMin_price(MinPrice_inputSignal)
+	if asset.Min_price != 2.0 {
+		t.Errorf("expected Min_Price to be 2.0, got %f", asset.Min_price)
+	}
+	//call and test Max_price
+	asset.setMax_price(MaxPrice_inputSignal)
+	if asset.Max_price != 12.0 {
+		t.Errorf("expected Max_Price to be 12.0, got %f", asset.Max_price)
+	}
+	// call and test Desired_temp
+	asset.setDesired_temp(DesTemp_inputSignal)
+	if asset.Desired_temp != 23.7 {
+		t.Errorf("expected Desierd temprature is to be 23.7, got %f", asset.Desired_temp)
+	}
+
+}
+
+func Test_GetMethods(t *testing.T) {
+
+	uasset := initTemplate().(*UnitAsset)
+
+	////MinTemp////
+	// check if the value from the struct is the acctual value that the func is getting
+	result := uasset.getMin_temp()
+	if result.Value != uasset.Min_temp {
+		t.Errorf("expected Value of the min_temp is to be %v, got %v", uasset.Min_temp, result.Value)
+	}
+	//check that the Unit is correct
+	if result.Unit != "Celsius" {
+		t.Errorf("expected Unit to be 'Celsius', got %v", result.Unit)
+
+	}
+	////MaxTemp////
+	result2 := uasset.getMax_temp()
+	if result2.Value != uasset.Max_temp {
+		t.Errorf("expected Value of the Max_temp is to be %v, got %v", uasset.Max_temp, result2.Value)
+	}
+	//check that the Unit is correct
+	if result2.Unit != "Celsius" {
+		t.Errorf("expected Unit of the Max_temp is to be 'Celsius', got %v", result2.Unit)
+	}
+	////MinPrice////
+	// check if the value from the struct is the acctual value that the func is getting
+	result3 := uasset.getMin_price()
+	if result3.Value != uasset.Min_price {
+		t.Errorf("expected Value of the minPrice is to be %v, got %v", uasset.Min_price, result3.Value)
+	}
+	//check that the Unit is correct
+	if result3.Unit != "SEK" {
+		t.Errorf("expected Unit to be 'SEK', got %v", result3.Unit)
+	}
+
+	////MaxPrice////
+	// check if the value from the struct is the acctual value that the func is getting
+	result4 := uasset.getMax_price()
+	if result4.Value != uasset.Max_price {
+		t.Errorf("expected Value of the maxPrice is  to be %v, got %v", uasset.Max_price, result4.Value)
+	}
+	//check that the Unit is correct
+	if result4.Unit != "SEK" {
+		t.Errorf("expected Unit to be 'SEK', got %v", result4.Unit)
+	}
+	////DesierdTemp////
+	// check if the value from the struct is the acctual value that the func is getting
+	result5 := uasset.getDesired_temp()
+	if result5.Value != uasset.Desired_temp {
+		t.Errorf("expected desired temprature is to be %v, got %v", uasset.Desired_temp, result5.Value)
+	}
+	//check that the Unit is correct
+	if result5.Unit != "Celsius" {
+		t.Errorf("expected Unit to be 'Celsius', got %v", result5.Unit)
+	}
+	////SEK_Price////
+	result6 := uasset.getSEK_price()
+	if result6.Value != uasset.SEK_price {
+		t.Errorf("expected electric price is to be %v, got %v", uasset.SEK_price, result6.Value)
+	}
+}
+
+func Test_initTemplet(t *testing.T) {
+	uasset := initTemplate().(*UnitAsset)
+	/*
+		name := uasset.GetName()
+		Services := uasset.GetServices()
+		Cervices := uasset.GetCervices()
+		Details := uasset.GetDetails()
+	*/
+
+	//// unnecessary test, but good for practicing
+
+	name := uasset.GetName()
+	if name != "Set Values" {
+		t.Errorf("expected name of the resource is %v, got %v", uasset.Name, name)
+	}
+	Services := uasset.GetServices()
+	if Services == nil {
+		t.Fatalf("If Services is nil, not worth to continue testing")
+	}
+	//Services//
+	if Services["SEK_price"].Definition != "SEK_price" {
+		t.Errorf("expected service defenition to be SEKprice")
+	}
+	if Services["max_temperature"].Definition != "max_temperature" {
+		t.Errorf("expected service defenition to be max_temperature")
+	}
+	if Services["min_temperature"].Definition != "min_temperature" {
+		t.Errorf("expected service defenition to be min_temperature")
+	}
+	if Services["max_price"].Definition != "max_price" {
+		t.Errorf("expected service defenition to be max_price")
+	}
+	if Services["min_price"].Definition != "min_price" {
+		t.Errorf("expected service defenition to be min_price")
+	}
+	if Services["desired_temp"].Definition != "desired_temp" {
+		t.Errorf("expected service defenition to be desired_temp")
+	}
+	//GetCervice//
+	Cervices := uasset.GetCervices()
+	if Cervices != nil {
+		t.Fatalf("If cervises not nil, not worth to continue testing")
+	}
+	//Testing Details//
+	Details := uasset.GetDetails()
+	if Details == nil {
+		t.Errorf("expected a map, but Details was nil, ")
+	}
+
+}
+
+func Test_newUnitAsset(t *testing.T) {
+	// prepare for graceful shutdown
+	ctx, cancel := context.WithCancel(context.Background()) // create a context that can be cancelled
+	defer cancel()                                          // make sure all paths cancel the context to avoid context leak
+
+	// instantiate the System
+	sys := components.NewSystem("Comfortstat", ctx)
+
+	// Instatiate the Capusle
+	sys.Husk = &components.Husk{
+		Description: " is a controller for a consumed servo motor position based on a consumed temperature",
+		Certificate: "ABCD",
+		Details:     map[string][]string{"Developer": {"Arrowhead"}},
+		ProtoPort:   map[string]int{"https": 0, "http": 8670, "coap": 0},
+		InfoLink:    "https://github.com/lmas/d0020e_code/tree/master/Comfortstat",
+	}
+	setSEK_price := components.Service{
+		Definition:  "SEK_price",
+		SubPath:     "SEK_price",
+		Details:     map[string][]string{"Unit": {"SEK"}, "Forms": {"SignalA_v1a"}},
+		Description: "provides the current electric hourly price (using a GET request)",
+	}
+
+	setMax_temp := components.Service{
+		Definition:  "max_temperature",
+		SubPath:     "max_temperature",
+		Details:     map[string][]string{"Unit": {"Celsius"}, "Forms": {"SignalA_v1a"}},
+		Description: "provides the maximum temp the user wants (using a GET request)",
+	}
+	setMin_temp := components.Service{
+		Definition:  "min_temperature",
+		SubPath:     "min_temperature",
+		Details:     map[string][]string{"Unit": {"Celsius"}, "Forms": {"SignalA_v1a"}},
+		Description: "provides the minimum temp the user could tolerate (using a GET request)",
+	}
+	setMax_price := components.Service{
+		Definition:  "max_price",
+		SubPath:     "max_price",
+		Details:     map[string][]string{"Unit": {"SEK"}, "Forms": {"SignalA_v1a"}},
+		Description: "provides the maximum price the user wants to pay (using a GET request)",
+	}
+	setMin_price := components.Service{
+		Definition:  "min_price",
+		SubPath:     "min_price",
+		Details:     map[string][]string{"Unit": {"SEK"}, "Forms": {"SignalA_v1a"}},
+		Description: "provides the minimum price the user wants to pay (using a GET request)",
+	}
+	setDesired_temp := components.Service{
+		Definition:  "desired_temp",
+		SubPath:     "desired_temp",
+		Details:     map[string][]string{"Unit": {"Celsius"}, "Forms": {"SignalA_v1a"}},
+		Description: "provides the desired temperature the system calculates based on user inputs (using a GET request)",
+	}
+
+	uac := UnitAsset{
+		//These fields should reflect a unique asset (ie, a single sensor with unique ID and location)
+		Name:         "Set Values",
+		Details:      map[string][]string{"Location": {"Kitchen"}},
+		SEK_price:    1.5,  // Example electricity price in SEK per kWh
+		Min_price:    1.0,  // Minimum price allowed
+		Max_price:    2.0,  // Maximum price allowed
+		Min_temp:     20.0, // Minimum temperature
+		Max_temp:     25.0, // Maximum temprature allowed
+		Desired_temp: 0,    // Desired temp calculated by system
+		Period:       15,
+
+		// maps the provided services from above
+		ServicesMap: components.Services{
+			setMax_temp.SubPath:     &setMax_temp,
+			setMin_temp.SubPath:     &setMin_temp,
+			setMax_price.SubPath:    &setMax_price,
+			setMin_price.SubPath:    &setMin_price,
+			setSEK_price.SubPath:    &setSEK_price,
+			setDesired_temp.SubPath: &setDesired_temp,
+		},
+	}
+
+	ua, _ := newUnitAsset(uac, &sys, nil)
+
+	name := ua.GetName()
+	if name != "Set Values" {
+		t.Errorf("expected name to be Set values, but got: %v", name)
+	}
+
+}
+
+func Test_calculateDesiredTemp(t *testing.T) {
+	var True_result float64 = 22.5
+	asset := initTemplate().(*UnitAsset)
+
+	result := asset.calculateDesiredTemp()
+
+	if result != True_result {
+		t.Errorf("Expected calculated temp is %v, got %v", True_result, result)
+	}
+}
+
+func Test_specialcalculate(t *testing.T) {
+	asset := UnitAsset{
+		SEK_price: 3.0,
+		Max_price: 2.0,
+		Min_temp:  17.0,
+	}
+
+	result := asset.calculateDesiredTemp()
+
+	if result != asset.Min_temp {
+		t.Errorf("Expected temperature to be %v, got %v", asset.Min_temp, result)
+	}
+}
+
+// Fuctions that help creating bad body
+type errReader int
+
+var errBodyRead error = fmt.Errorf("bad body read")
+
+func (errReader) Read(p []byte) (n int, err error) {
+	return 0, errBodyRead
+}
+
+func (errReader) Close() error {
+	return nil
+}
+
+var brokenURL string = string([]byte{0x7f})
+
+func TestGetAPIPriceData(t *testing.T) {
+	priceExample = fmt.Sprintf(`[{
+		"SEK_per_kWh": 0.26673,
+		"EUR_per_kWh": 0.02328,
+		"EXR": 11.457574,
+		"time_start": "%d-%02d-%02dT%02d:00:00+01:00",
+		"time_end": "2025-01-06T04:00:00+01:00"
+		}]`, time.Now().Local().Year(), int(time.Now().Local().Month()), time.Now().Local().Day(), time.Now().Local().Hour(),
+	)
+
+	fakeBody := fmt.Sprintf(priceExample)
+	resp := &http.Response{
+		Status:     "200 OK",
+		StatusCode: 200,
+		Body:       io.NopCloser(strings.NewReader(fakeBody)),
+	}
+
+	// Testing good cases
+
+	// Test case: goal is no errors
+	url := fmt.Sprintf(
+		`https://www.elprisetjustnu.se/api/v1/prices/%d/%02d-%02d_SE1.json`,
+		time.Now().Local().Year(), int(time.Now().Local().Month()), time.Now().Local().Day(),
+	)
+	newMockTransport(resp)
+	err := getAPIPriceData(url)
+	if err != nil {
+		t.Errorf("expected no errors but got %s :", err)
+	}
+
+	// Check if the correct price is stored
+	expectedPrice := 0.26673
+
+	if globalPrice.SEK_price != expectedPrice {
+		t.Errorf("Expected SEK_price %f, but got %f", expectedPrice, globalPrice.SEK_price)
+	}
+
+	// Testing bad cases
+
+	// Test case: using wrong url leads to an error
+	newMockTransport(resp)
+	// Call the function (which now hits the mock server)
+	err = getAPIPriceData(brokenURL)
+	if err == nil {
+		t.Errorf("Expected an error but got none!")
+	}
+
+	// Test case: if reading the body causes an error
+	resp.Body = errReader(0)
+	newMockTransport(resp)
+	err = getAPIPriceData(url)
+	if err != errBodyRead {
+		t.Errorf("expected an error %v, got %v", errBodyRead, err)
+	}
+
+	//Test case: if status code > 299
+	resp.Body = io.NopCloser(strings.NewReader(fakeBody))
+	resp.StatusCode = 300
+	newMockTransport(resp)
+	err = getAPIPriceData(url)
+
+	if err != err_statuscode {
+		t.Errorf("expected an bad status code but got %v", err)
+
+	}
+
+	// test case: if unmarshal a bad body creates a error
+	resp.StatusCode = 200
+	resp.Body = io.NopCloser(strings.NewReader(fakeBody + "123"))
+	newMockTransport(resp)
+	err = getAPIPriceData(url)
+
+	if err == nil {
+		t.Errorf("expected an error, got %v :", err)
+	}
+
+}
